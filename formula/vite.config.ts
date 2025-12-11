3d97bc55c7a35dfbe07cab470ace5e4d52b66b94
vostroslavaa <v.osrtoslava@icloud.comB>
2025-12-11 10:07:35 +0300

    Fix: Build Formula as single HTML file for file:// protocol support

--- formula/vite.config.ts
+++ formula/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from "vite-plugin-singlefile"
 import { fileURLToPath } from 'url'
 import { dirname, resolve } from 'path'
 
@@ -8,7 +9,7 @@ const __dirname = dirname(__filename)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), viteSingleFile()],
     base: './',
     resolve: {
         alias: {
